c77334a129764fc223ee2195abbc6e6d3a1a4af5
HartejAnand <76600578+HartejAnand@users.noreply.github.com>
2021-04-19 12:46:35 -0400

    Add files via upload

---
+++ drop.js
@@ -0,0 +1,31 @@
+class Drop{
+    constructor(x, y) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.circle(x, y, 2,  options);
+        this.radius = 2;
+        World.add(world, this.body);
+      }
+      
+      display(){
+       
+        if(this.body.position.y>windowHeight){
+            Matter.Body.setPosition(this.body,{x:random(0,windowWidth), y:random(-10,0)})
+        }
+        
+        
+      }
+  
+      showDrop = function(){
+        
+  
+        noStroke();
+        fill(100,100,255);
+        ellipseMode(RADIUS);
+        ellipse(this.body.position.x, this.body.position.y, this.radius, this.radius);
+      };
+  }
+  
